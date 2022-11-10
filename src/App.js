14abe454c41a3aee0d5fec3cdbce7287954bf581
Ronald George <ronaldsamgeorge@gmail.com>
2022-11-10 20:19:20 +0530

    Feature : Alert on adding same named contact

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import { useState } from "react";
 
 const App = () => {
 
-  const [persons, setPersons] = useState([{name: "John Doe"}]);
+  const [persons, setPersons] = useState([{name: "John Doe"}]);  // array of Objects to store contacts
   const [newName, setNewName] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
-    const newContactObject = {
-      name : newName
+
+    if(personExists(newName)){
+      alert("Person already in the phonebook");
+      return;
     }
 
-    setPersons(persons.concat(newContactObject));
-    setNewName("");
-    console.log(persons)
+    console.log(personExists(newName));
+    const newContactObject = {                                  // object to hold form input
+      name : newName                                  
+    } 
+    setPersons(persons.concat(newContactObject));               // concat returns a new array,
+    setNewName("");                                             // to clear the input field on submitting
   }
 
   const nameEntered = (event) => {
-    setNewName(event.target.value);
-    console.log(event.target.value);
+    setNewName(event.target.value);                             // capture the input as it is being typed 
+  }
+
+  const personExists = (searchName) => {
+    var isPresent = false;
+    persons.forEach(person => {
+      if (person.name === searchName){
+         isPresent= true;
+      }
+    })
+    return isPresent;
   }
 
   return(
@@ -28,7 +42,6 @@ const App = () => {
         <div>
           name : <input value={newName} onChange={nameEntered}/>  
         </div>
-        
         <div>
           <button type="Submit" >Save Name</button>
         </div>
